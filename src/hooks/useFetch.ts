4fc857ec5cf63c1717ef4e54343387132c30c440
Luis Eugenio SC <luis.saldarriaga@hass-colombia.com>
2022-12-22 13:38:50 -0500

    Add documentation on hooks

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Hook para ejecutar las consultas GQL
+ * @param {String} query Recibe la consulta GQL a ejecutar
+ * @param  {Object} [variables] Recibe las variables de la consulta GQL a ejecutar.
+ * @returns {Object} Retorna un objeto con la data, si está cargando, si sucede un error y la función refetch para volver a ejecutar la consulta.
+ */
 const useFetch = ({
   query,
   variables = {},
@@ -10,6 +16,7 @@ const useFetch = ({
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
   const refetch = () => {
     setLoading(true);
     fetch(`https://rickandmortyapi.com/graphql`, {
--- src/hooks/useNav.ts
+++ src/hooks/useNav.ts
@@ -5,10 +5,13 @@ import {
   useNavigate,
 } from "react-router-dom";
 
+/**
+ * Hook para navegar entre las rutas
+ * @returns Retorna una función que como primer parametro recibe un string a la ruta a la que se va a dirigir y como segundo parametro las diferentes opciones.
+ */
 const useNav = () => {
   const { search } = useLocation();
   const navigate = useNavigate();
-
   return (to: To, opts?: NavigateOptions) =>
     navigate(
       typeof to == "string"
