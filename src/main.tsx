d12d6a10bebc5972df09a1fa60acb23cfa84e51d
Luis Eugenio SC <luis.saldarriaga@hass-colombia.com>
2022-12-22 13:40:37 -0500

    Add theme for mui and providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#379237",
+    },
+    secondary: {
+      main: "#9cff2e",
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
